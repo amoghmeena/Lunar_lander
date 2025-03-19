98817b79bb86ffec8604f283fa7c5c006826c48e
AMOGH NARAYAN MEENA <amogh2004meena@gmail.com>
2025-03-18 21:30:59 -0400

    changes

--- Lunar_lander/main.cpp
+++ Lunar_lander/main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Amogh Meena
+* Assignment: Lunar Lander
+* Date due: 2025-03-18, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define LOG(argument) std::cout << argument << '\n'
 #define STB_IMAGE_IMPLEMENTATION
 #define GL_SILENCE_DEPRECATION
